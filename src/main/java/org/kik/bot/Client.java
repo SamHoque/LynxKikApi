5fb297c16665c3995dd0cd541a9f0fa4a3d4985a
iservecharlie <iservecharlie@gmail.com>
2019-09-23 17:25:35 +0000

    added logging framework

--- src/main/java/org/kik/bot/Client.java
+++ src/main/java/org/kik/bot/Client.java
@@ -1,12 +1,14 @@
 package org.kik.bot;
 
 import net.lynx.client.KikClient;
+import org.apache.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Client implements CommandLineRunner {
+    private static final Logger logger = Logger.getLogger(Client.class);
     public static void main(String[] args) {
         SpringApplication.run(Client.class, args);
     }
@@ -18,7 +20,7 @@ public class Client implements CommandLineRunner {
             kikClient.login_to_kik_server(args[0], args[1]);
             kikClient.start();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Failed to start KikClient", e);
         }
     }
 }
